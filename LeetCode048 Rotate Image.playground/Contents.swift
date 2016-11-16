c455e135bdccf36496ea6717e9088330638eb8c7
GhostZephyrus <wangrenzhu@qq.com>
2016-11-16 10:55:23 +0800

    045 JumpGame

--- LeetCode048 Rotate Image.playground/Contents.swift
+++ LeetCode048 Rotate Image.playground/Contents.swift
@@ -1,25 +1,3 @@
-//public class Solution {
-//    public void rotate(int[][] matrix) {
-//    int row,col;
-//    int length = matrix.length;
-//    int edge = length-1;
-//    int nEdge;
-//    
-//    for(row = 0;row<edge;row++){
-//    nEdge = edge;
-//    for(col = row;col<edge;col++,nEdge--){
-//    int val=matrix[row][col];
-//    matrix[row][col] = matrix[nEdge][row];
-//    matrix[nEdge][row] = matrix[edge][nEdge];
-//    matrix[edge][nEdge] = matrix[col][edge];
-//    matrix[col][edge] = val;
-//    }
-//    edge--;
-//    }
-//    }
-//}
-
-
 class Solution {
     func rotate(_ matrix: inout [[Int]]) {
         let length = matrix.count
